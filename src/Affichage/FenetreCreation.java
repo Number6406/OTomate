902b7c13114c87af5c84ea31d586404cae53cdb1
Gilles B <bonhoure.gilles@gmail.com>
2016-06-09 21:54:41 +0200

    nouvelle maj du formulaire

--- src/Affichage/FenetreCreation.java
+++ src/Affichage/FenetreCreation.java
@@ -9,14 +9,20 @@ import Otomate.Joueur;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 /**
  * Fenêtre de création de personnes et choix des automates en prévision de début de partie
@@ -28,6 +34,8 @@ public class FenetreCreation extends FenetreBase {
     
     // Elements SWING
     JTabbedPane ongletsJoueurs = new JTabbedPane();
+    
+    List<JPanel> panelJ = new ArrayList<JPanel>();
     
     JPanel pan_b = new JPanel(new BorderLayout());
     JButton bValider = new JButton("Commencer");
@@ -48,23 +56,52 @@ public class FenetreCreation extends FenetreBase {
         this.add(ongletsJoueurs, BorderLayout.CENTER);
         
         // Pour tous les joueurs, leur créer un onglet
-        for(int i=0; i<nbJ; i++) {
-            JPanel panelJ = new JPanel(new GridBagLayout());
+        for(int i=1; i<=nbJ; i++) {
+            JPanel pJoueur = new JPanel(new GridBagLayout());
+            panelJ.add(pJoueur);
+            
             GridBagConstraints c = new GridBagConstraints();
-            c.gridwidth = 0;
-            c.gridheight = 0;
+            c.gridwidth = 1;
+            c.gridheight = 1;
             c.gridx = 0;
             c.gridy = 0;
             c.weightx = 0;
             c.weighty = 0;
             c.fill = GridBagConstraints.NONE;
             
-            ongletsJoueurs.add(panelJ);
+            ongletsJoueurs.add("Joueur" + i, pJoueur);
+            JTextField userName = new JTextField("Joueur " + i);
+            c.gridy = 1;
+            pJoueur.add(userName, c);
+            
+            c.gridy = 2;
+            pJoueur.add(new JLabel("Automate Méchant : "), c);
+            c.gridx = 1;
+            c.weightx = 1;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            JTextField textAutoM = new JTextField("chemin");
+            pJoueur.add(textAutoM, c);
+            c.gridx = 2;
+            c.weightx = 0;
+            c.fill = GridBagConstraints.NONE;
+            JButton bFichierM = new JButton("Fichier");
+            pJoueur.add(bFichierM, c);
+            bFichierM.addActionListener((ActionEvent e) -> {
+                JFileChooser fileM = new JFileChooser();
+                int returnValue = fileM.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileM.getSelectedFile();
+                    textAutoM.setText(selectedFile.getAbsolutePath());
+                }
+            });
+            
+            c.gridy = 3;
+            pJoueur.add(new JLabel("Automates Gentils : "), c);
         }
         
         this.add(pan_b, BorderLayout.SOUTH);
-        this.add(bAnnuler);
-        this.add(bValider);
+        pan_b.add(bAnnuler, BorderLayout.WEST);
+        pan_b.add(bValider, BorderLayout.EAST);
         
     }
     
