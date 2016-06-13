b34cc94c3f815c15b5c26d21f45919eb92a0c565
Stractus <charles-marchand@hotmail.fr>
2016-06-13 10:55:50 +0200

    Le merge c'est vraiment de la merde

--- src/Otomate/Joueur.java
+++ src/Otomate/Joueur.java
@@ -10,14 +10,13 @@ import java.util.List;
  *
  */
 public class Joueur {
-<<<<<<< HEAD
     String name;
     boolean mechant;
     private Color couleur;
     //Attributs
-    private List<$Personnage> personnages;
-    
-    //M�thodes
+private List<$Personnage> personnages;
+
+//M�thodes
     public Joueur(List<String> xmls, boolean estMechant, int nbMechants) {
     	personnages = new LinkedList<>();
     	mechant = estMechant;
@@ -53,44 +52,4 @@ public class Joueur {
 	public void setCouleur(Color couleur) {
 		this.couleur = couleur;
 	}
-    
-=======
-	String name;
-	boolean mechant;
-	// Attributs
-	private List<$Personnage> personnages;
-
-	// M�thodes
-	public Joueur(List<String> xmls, boolean estMechant, int nbMechants) {
-		personnages = new LinkedList<>();
-		mechant = estMechant;
-		if (mechant) {
-			personnages.add(new Mechant(xmls.get(0)));
-			for (int i = 1; i < nbMechants; i++) {
-				personnages.add(new Mechant(personnages.get(0)));
-			}
-		} else {
-			for (int i = 0; i < xmls.size(); i++) {
-				personnages.add(new Gentil(xmls.get(i)));
-			}
-		}
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public List<$Personnage> getPersonnages() {
-		return personnages;
-	}
-
-	public $Personnage getPersonnagesI(int i) {
-		return personnages.get(i);
-	}
-
-	public int getSizePersonnages() {
-		return personnages.size();
-	}
-
->>>>>>> master
 }
