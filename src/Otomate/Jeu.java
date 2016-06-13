8d93a87cc39e920422c8afbe73f34bffea555b7a
KyZoar <marion.pellicer@laposte.net>
2016-06-13 11:27:58 +0200

    Voilà on touche plus please

--- src/Otomate/Jeu.java
+++ src/Otomate/Jeu.java
@@ -14,25 +14,15 @@ import java.util.Random;
 import javax.swing.Timer;
 
 public class Jeu {
-<<<<<<< HEAD
-    
-    //Attributs
-public static Grille plateau;
-public static List<Joueur> joueurs;
-public static List<Integer> refPersos;
-public static Historique historique;
-//private static List<Conditions2> listCond;
-private static List<Objet> listCont;
-=======
->>>>>>> master
+
 
 	// Attributs
 	public static Grille plateau;
 	public static List<Joueur> joueurs;
 	public static List<Integer> refPersos;
 	public static Historique historique;
-	private static List<Conditions2> listCond;
-	private static List<Objet> listCont;
+	//private static List<Conditions2> listCond;
+	//private static List<Objet> listCont;
 
 	// Methodes
 
@@ -104,82 +94,7 @@ private static List<Objet> listCont;
 		}
 	}
 
-	public static void main(String[] pArgs) throws InterruptedException {
-		plateau = new Grille();
-		historique = new Historique();
-		// Variables définies grâce au menu d'affichage ->
-		int nbJoueurs = 2;
-		int nbPersoParJoueur = 2;
-		int nZombie = 1; // Variable possiblement tirée au sort
-		int nbPersoParZombie = 2;
-		List<String> xmlsGentils = new LinkedList<String>();
-		List<String> xmlsMechants = new LinkedList<String>();
-		List<List<String>> xmls = new LinkedList<>();
-		xmls.add(xmlsGentils);
-		xmls.add(xmlsMechants);
-		// <- Fin variables
-		int j, p;
-		initJoueurs(nbPersoParZombie, nZombie, xmls);
-		refPersos = new LinkedList<Integer>();
-		String tempHistorique;
-		Grille.initialisergrille(joueurs);
-		Affichage.charger();
-		int nbTotal = (nbJoueurs - 1) * nbPersoParJoueur + ((nbJoueurs - 1) * nbPersoParJoueur / nbPersoParZombie);
-		while (!finPartie()) {
-			melange();
-			historique.addTour();
-			for (int i = 0; i < refPersos.size(); i++) {
-				j = refPersos.get(i) / 100;
-				p = refPersos.get(i) % 100;
-				if (joueurs.get(j).getPersonnagesI(p) instanceof Gentil) {
-					Gentil gentilperso = ((Gentil) joueurs.get(j).getPersonnagesI(p));
-					while (gentilperso.getParalysie() > 0) {
-						if (gentilperso.getEfdrogue() != 0) {
-							if (gentilperso.getDrogue() == 3) {
-								gentilperso.setVie(gentilperso.getVie() + 5);
-								if (gentilperso.getVie() > gentilperso.getViemax()) {
-									gentilperso.setVie(gentilperso.getViemax());
-								}
-							}
-							if (gentilperso.getDrogue() == 4) {
-								gentilperso.setVie(gentilperso.getVie() - 5);
-							}
-							if (gentilperso.getDrogue() == 5) {
-								gentilperso.setParalysie(2);
-							}
-							if (gentilperso.getDrogue() == 6) {
-								gentilperso.setParalysie(0);
-							}
-							// ((Gentil) p).efdrogue --; PENSER A LE METTRE A LA
-							// FIN DE LA GRANDE BOUCE DE TOUR
-						} else {
-							gentilperso.setDrogue(0);
-						}
-						tempHistorique = joueurs.get(j).getPersonnagesI(p).jouer(listCond, plateau, listCont, joueurs);
-						historique.ceTour().addEvenement(new Evenement(gentilperso, tempHistorique));
-						((Gentil) joueurs.get(j).getPersonnagesI(p))
-								.setParalysie(((Gentil) joueurs.get(j).getPersonnagesI(p)).getParalysie() - 1);
-						Thread.sleep(200);
-					}
-				} else {
-					tempHistorique = joueurs.get(j).getPersonnagesI(p).jouer(listCond, plateau, listCont, joueurs);
-					historique.ceTour().addEvenement(new Evenement(joueurs.get(j).getPersonnagesI(p), tempHistorique));
-				}
-				// tempHistorique sera la chaîne renvoyée par l'action d'un
-				// joueu
-				// $Personnage persoCourant =
-				// joueurs.get(refPersos.get(i)/100).getPersonnagesI(refPersos.get(i)-(refPersos.get(i)/100));
-				// tempHistorique = persoCourant.jouer();
-				// tempHistorique sera la chaine renvoyee par l'action d'un
-				// joueur
-			}
-			Thread.sleep(200);
-
-		}
-	}
-<<<<<<< HEAD
-}
-		
+	
     public static void main(String[] pArgs) throws InterruptedException {
     	plateau = new Grille();
     	historique = new Historique();
@@ -261,6 +176,4 @@ private static List<Objet> listCont;
     		
     	}
     }
-=======
->>>>>>> master
 }
