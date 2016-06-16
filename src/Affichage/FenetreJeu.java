3b2bc90d4a5381ce5dab548e5106e55b84ec127d
Talenie <alicia.abonnenc@hotmail.com>
2016-06-16 16:51:02 +0200

    euh chai pas

--- src/Affichage/FenetreJeu.java
+++ src/Affichage/FenetreJeu.java
@@ -381,14 +381,13 @@ public class FenetreJeu extends JFrame {
                         String cons = Jeu.univers.getObjets().get(((Gentil) p).getInventaire()).getPath();
                         consommable = new ImageIcon(getClass().getResource(cons));
                     }
-                    if(p instanceof Mechant)
+                }
+                if(p instanceof Mechant){
                     if(((Mechant) p).getInventaire() != 0) {
                     	System.err.println("Le joueur a� un item dans l'inventaire !");
-                        String cons = Jeu.univers.getObjets().get(((Gentil) p).getInventaire()).getPath();
+                        String cons = Jeu.univers.getObjets().get(((Mechant) p).getInventaire()).getPath();
                         consommable = new ImageIcon(getClass().getResource(cons));
                     }
-                    
-                    
                 }
                 
                 // Mise à jour de l'affichage
